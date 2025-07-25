9abbea29c55e360642112f8fee816133c1c6aff9
Chris <120978532+spicybung@users.noreply.github.com>
2025-07-24 22:37:45 -0400

    Parse Chinatown Wars Vertex Buffer
    seems to be a division factor of 64.0, and some hocus pocus.

--- gtaLib/mdl_cw.py
+++ gtaLib/mdl_cw.py
@@ -25,16 +25,18 @@ from bpy.props import StringProperty
 from bpy_extras.io_utils import ImportHelper
 
 #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #
-#   This script is for .MDL/.WBLs - the format for models in Chinatown Wars PSP/DS
+#   This script is for .mdl/.wbls - formats for models in GTA:CW Mobile/PSP/DS      #
 #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #
 # - Script resources:
-# • https://gtamods.com/wiki/Leeds_Engine
+# • https://gtamods.com/wiki/Leeds_Engine (some assets point to R* Leeds making CW in Unity?)
 # • https://gtamods.com/wiki/MDL (TODO: update stub to include Chinatown Wars documentation)
-# • https://web.archive.org/web/20221108130633/http://gtamodding.ru/wiki/GAME.PAK_(Chinatown_Wars)#.D0.9C.D0.BE.D0.B4.D0.B5.D0.BB.D0.B8 (Russian)
-# • https://web.archive.org/web/20221108130633/http://gtamodding.ru/wiki/GAME.PAK_(Chinatown_Wars)?_x_tr_sl=ru&_x_tr_tl=en&_x_tr_hl=en (English)
+# • https://github.com/aap/librwgta (*re'd RW/Leeds Engine source by The_Hero*)
+# • https://web.archive.org/web/20221108130633/http://gtamodding.ru/wiki/GAME.PAK_(Chinatown_Wars)#.D0.9C.D0.BE.D0.B4.D0.B5.D0.BB.D0.B8 (*Russian*)
+# • https://web.archive.org/web/20221108130633/http://gtamodding.ru/wiki/GAME.PAK_(Chinatown_Wars)?_x_tr_sl=ru&_x_tr_tl=en&_x_tr_hl=en (*English*)
 # - Mod resources/cool stuff:
 # • https://gtaforums.com/topic/781150-relctw-chinatown-wars-mobile-resource-explorer/
 # • https://web.archive.org/web/20221005045615/https://github.com/DK22Pac/ctw-gxt-tools (in case the repo goes down)
+# • https://www.ign.com/articles/2009/02/25/rockstar-leeds-talks-gta-ds ( ...it's IGN, but old IGN at least)
 
 #######################################################
 def print_bytes(data, start=0, end=None):
@@ -64,9 +66,54 @@ def read_vec3_int4096(data, offset):
     z = struct.unpack_from('<i', data, offset + 8)[0] / 4096.0
     return (x, y, z)
 #######################################################
+def read_leeds_cw_transform(data, offset):
+    # Diagonal: 32-bit int
+    RightX = struct.unpack_from('<i', data, offset)[0] / 4096.0
+    # Off-diagonal: 16-bit int
+    RightY = struct.unpack_from('<h', data, offset+4)[0] / 4096.0
+    RightZ = struct.unpack_from('<h', data, offset+6)[0] / 4096.0
+    TopX   = struct.unpack_from('<h', data, offset+8)[0] / 4096.0
+    # Diagonal: 32-bit int
+    TopY   = struct.unpack_from('<i', data, offset+0x0A)[0] / 4096.0
+    TopZ   = struct.unpack_from('<h', data, offset+0x0E)[0] / 4096.0
+    AtX    = struct.unpack_from('<h', data, offset+0x10)[0] / 4096.0
+    AtY    = struct.unpack_from('<h', data, offset+0x12)[0] / 4096.0
+    # Diagonal: 32-bit int
+    AtZ    = struct.unpack_from('<i', data, offset+0x14)[0] / 4096.0
+    # Padding: 32-bit int
+    Padding = struct.unpack_from('<i', data, offset+0x18)[0]
+    # Position (translation): 32-bit ints
+    PosnX = struct.unpack_from('<i', data, offset+0x1C)[0] / 4096.0
+    PosnY = struct.unpack_from('<i', data, offset+0x20)[0] / 4096.0
+    PosnZ = struct.unpack_from('<i', data, offset+0x24)[0] / 4096.0
+
+    print("    -Transform")
+    print(f"      RightX {RightX:.6f}")
+    print(f"      RightY {RightY:.6f}")
+    print(f"      RightZ {RightZ:.6f}")
+    print(f"      TopX   {TopX:.6f}")
+    print(f"      TopY   {TopY:.6f}")
+    print(f"      TopZ   {TopZ:.6f}")
+    print(f"      AtX    {AtX:.6f}")
+    print(f"      AtY    {AtY:.6f}")
+    print(f"      AtZ    {AtZ:.6f}")
+    print(f"      Padding {Padding}")
+    print(f"      PosnX  {PosnX:.6f}")
+    print(f"      PosnY  {PosnY:.6f}")
+    print(f"      PosnZ  {PosnZ:.6f}")
+
+    # Return as a dictionary
+    return {
+        "Right": (RightX, RightY, RightZ),
+        "Top":   (TopX, TopY, TopZ),
+        "At":    (AtX, AtY, AtZ),
+        "Pos":   (PosnX, PosnY, PosnZ),
+        "Padding": Padding
+    }
+#######################################################
 class ImportWBLPSPSectorOperator(bpy.types.Operator, ImportHelper):
     bl_idname = "import_scene.wbl_psp_sector"
-    bl_label = "Import WBL Sectors (.wbl, PSP float32, BLeeds style)"
+    bl_label = "Import Chinatown Wars WBL (.wbl)"
     filename_ext = ".wbl"
     filter_glob: StringProperty(default="*.wbl", options={'HIDDEN'})
 
@@ -75,40 +122,11 @@ class ImportWBLPSPSectorOperator(bpy.types.Operator, ImportHelper):
         try:
             with open(self.filepath, "rb") as f:
                 file_bytes = f.read()
-            header = file_bytes[:0x28]
-            print("==== .WBL HEADER HEX DUMP (0x00 - 0x27) ====")
-            print_bytes(header, 0, len(header))
-            print("==== BEGIN HEADER FIELD INTERPRETATION ====")
-            offset = 0
-            while offset < len(header):
-                if offset + 4 <= len(header):
-                    val_u32 = struct.unpack_from("<I", header, offset)[0]
-                    print(f"[0x{offset:02X}] u32: {padhex(val_u32)} ({val_u32})")
-                if offset + 2 <= len(header):
-                    val_u16 = struct.unpack_from("<H", header, offset)[0]
-                    print(f"[0x{offset:02X}] u16: {padhex(val_u16,4)} ({val_u16})")
-                val_u8 = header[offset]
-                print(f"[0x{offset:02X}] u8: {val_u8:02X} ({val_u8})")
-                offset += 4
+            print("==== .WBL TRANSFORM HEADER (0x00 - 0x27) ====")
+            header_transform = read_leeds_cw_transform(file_bytes, 0x00)
+            PosnX, PosnY, PosnZ = header_transform["Pos"]
 
-            print("==== 2-BYTE WORD VIEW (header) ====")
-            for o in range(0, len(header), 2):
-                if o + 2 <= len(header):
-                    val = struct.unpack_from("<H", header, o)[0]
-                    print(f"[0x{o:02X}] u16: {padhex(val,4)} ({val})")
-            print("==== 1-BYTE BYTE VIEW (header) ====")
-            for o in range(0, len(header)):
-                val = header[o]
-                print(f"[0x{o:02X}] u8: {val:02X} ({val})")
-            print("==== FLOAT VIEW (header, every 4 bytes) ====")
-            for o in range(0, len(header) - 3, 4):
-                try:
-                    val = struct.unpack_from("<f", header, o)[0]
-                    print(f"[0x{o:02X}] float: {val}")
-                except struct.error:
-                    pass
-
-            # --- Sectors: Each sector is 12 bytes, start at 0x28
+            # --- Sectors A to D: Each sector is 12 bytes, start at 0x28
             sector_base = 0x28
             mesh_offsets_found = set()
             for sector_idx in range(4):
@@ -128,7 +146,7 @@ class ImportWBLPSPSectorOperator(bpy.types.Operator, ImportHelper):
 
                 print(f"\n==== SECTOR {sector_idx} (0x{sector_ofs:02X} - 0x{sector_ofs+11:02X}) ====")
                 print(f"[0x{sector_ofs:02X}] Sector raw bytes: {' '.join(f'{b:02X}' for b in sec)}")
-                print(f"[0x{sector_ofs:02X}]   Bool1: {Bool1} (byte value: {sec[0]:02X})")
+                print(f"[0x{sector_ofs:02X}] Bool1: {Bool1} (byte value: {sec[0]:02X})")
                 print(f"[0x{sector_ofs+1:02X}] Bool2: {Bool2} (byte value: {sec[1]:02X})")
                 print(f"[0x{sector_ofs+2:02X}] NumInstances (int16): {NumInstances}")
                 print(f"[0x{sector_ofs+4:02X}] NumShadows   (int16): {NumShadows}")
@@ -161,15 +179,15 @@ class ImportWBLPSPSectorOperator(bpy.types.Operator, ImportHelper):
                 print(f"Instances:\n")
                 for instance_idx in range(NumInstances):
                     inst_base = level_ofs + instance_idx * 16
-                    ID = struct.unpack_from('<h', file_bytes, inst_base)[0]
-                    Instances = struct.unpack_from('<b', file_bytes, inst_base+2)[0]
+                    mdlID = struct.unpack_from('<h', file_bytes, inst_base)[0]
+                    RenderListID = struct.unpack_from('<b', file_bytes, inst_base+2)[0]
                     BuildingSwap = struct.unpack_from('<b', file_bytes, inst_base+3)[0]
                     ResourceID = struct.unpack_from('<I', file_bytes, inst_base+4)[0]
                     MeshOffset = struct.unpack_from('<I', file_bytes, inst_base+8)[0]
                     Pointer = struct.unpack_from('<I', file_bytes, inst_base+12)[0]
                     print(f"  Instance {instance_idx}:")
-                    print(f"    [0x{inst_base:02X}] ID (int16): {ID}")
-                    print(f"    [0x{inst_base+2:02X}] Instances (int8): {Instances}")
+                    print(f"    [0x{inst_base:02X}] ModelID (int16): {mdlID}")
+                    print(f"    [0x{inst_base+2:02X}] RenderListID (int8): {RenderListID}")
                     print(f"    [0x{inst_base+3:02X}] BuildingSwap (int8): {BuildingSwap}")
                     print(f"    [0x{inst_base+4:02X}] ResourceID (uint32): 0x{ResourceID:08X} ({ResourceID})")
                     print(f"    [0x{inst_base+8:02X}] MeshOffset (uint32): 0x{MeshOffset:08X} ({MeshOffset})")
@@ -200,9 +218,8 @@ class ImportWBLPSPSectorOperator(bpy.types.Operator, ImportHelper):
                 boundmin = read_vec3_int4096(file_bytes, MeshOffset+16)
                 boundmax = read_vec3_int4096(file_bytes, MeshOffset+28)
                 field_28 = struct.unpack_from('<I', file_bytes, MeshOffset+40)[0]
-                field_2C = struct.unpack_from('<I', file_bytes, MeshOffset+44)[0]
                 field_28f = struct.unpack_from('<f', file_bytes, MeshOffset+40)[0]
-                field_2Cf = struct.unpack_from('<f', file_bytes, MeshOffset+44)[0]
+                divFactor = struct.unpack_from('<f', file_bytes, MeshOffset+44)[0]
                 print(f"  [0x{MeshOffset:02X}] MDL Identifier: {ident_str} ('{mdl_ascii}')")
                 print(f"    [0x{MeshOffset+4:02X}] Unknown (int8): {unknown}")
                 print(f"    [0x{MeshOffset+5:02X}] numMaterials (int8): {numMaterials}")
@@ -212,7 +229,46 @@ class ImportWBLPSPSectorOperator(bpy.types.Operator, ImportHelper):
                 print(f"    [0x{MeshOffset+16:02X}] BoundMin (3 floats): ({boundmin[0]}, {boundmin[1]}, {boundmin[2]})")
                 print(f"    [0x{MeshOffset+28:02X}] BoundMax (3 floats): ({boundmax[0]}, {boundmax[1]}, {boundmax[2]})")
                 print(f"    [0x{MeshOffset+40:02X}] Field 28 (uint32): 0x{field_28:08X} ({field_28}) | As float: {field_28f}")
-                print(f"    [0x{MeshOffset+44:02X}] Field 2C (uint32): 0x{field_2C:08X} ({field_2C}) | As float: {field_2Cf}")
+                print(f"    [0x{MeshOffset+44:02X}] Division Factor?: {divFactor}") # bytes could be interpreted as 64.0, which it appears
+                                                                                    # to be a vertex buffer division factor(sounds weird)
+                                                                                    # ... or maybe it's a bitflag field
+            
+            # --- Vertex Buffer Parsing ---
+            # Chinatown Wars typically uses: pos, normals, texcoords
+            stride = 16  # Leeds/CW: 3x int16 (pos) + 3x int16 (normal) + 2x int16 (UV), therefore vertex stride is 16 bytes
+            vertex_base = MeshOffset + 48  # mesh header size = 0x30 (48 bytes)
+            print(f"    [0x{vertex_base:02X}] Vertex Buffer ({numVertices} entries, stride {stride}):")
+
+            for vi in range(numVertices):
+                v_off = vertex_base + vi * stride
+
+                # Position (signed 16-bit)
+                x_raw = struct.unpack_from('<h', file_bytes, v_off + 0)[0]
+                y_raw = struct.unpack_from('<h', file_bytes, v_off + 2)[0]
+                z_raw = struct.unpack_from('<h', file_bytes, v_off + 4)[0]
+                # Normals (signed 16-bit)
+                nx_raw = struct.unpack_from('<h', file_bytes, v_off + 6)[0]
+                ny_raw = struct.unpack_from('<h', file_bytes, v_off + 8)[0]
+                nz_raw = struct.unpack_from('<h', file_bytes, v_off +10)[0]
+                # UVs
+                u_raw  = struct.unpack_from('<h', file_bytes, v_off +12)[0]
+                v_raw  = struct.unpack_from('<h', file_bytes, v_off +14)[0]
+
+                # Decoding vert buffer
+                x = x_raw / 128.0
+                y = y_raw / 64.0
+                z = z_raw / 64.0
+                nx = nx_raw / 32767.0
+                ny = ny_raw / 32767.0
+                nz = nz_raw / 32767.0   # or 128.0 for normals? idk
+                u = u_raw / 2048.0
+                v = v_raw / 2048.0  # the most accurate I could get UVs so far
+
+                print(f"      Vertex {vi:3d}:")
+                print(f"        X:  offset 0x{v_off:06X}  bytes {file_bytes[v_off:v_off+2].hex(' ').upper()}  raw {x_raw:6d}  value {x:.6f}")
+                print(f"        Y:  offset 0x{v_off+2:06X}  bytes {file_bytes[v_off+2:v_off+4].hex(' ').upper()}  raw {y_raw:6d}  value {y:.6f}")
+                print(f"        Z:  offset 0x{v_off+4:06X}  bytes {file_bytes[v_off+4:v_off+6].hex(' ').upper()}  raw {z_raw:6d}  value {z:.6f}")
+                print(f"        Norm: ({nx:.6f}, {ny:.6f}, {nz:.6f})  UV: ({u:.6f}, {v:.6f})")
 
         except Exception as e:
             tb_str = traceback.format_exc()
@@ -224,7 +280,7 @@ class ImportWBLPSPSectorOperator(bpy.types.Operator, ImportHelper):
     
 #######################################################
 def menu_func_import(self, context):
-    self.layout.operator(ImportWBLPSPSectorOperator.bl_idname, text="R* Leeds Chinatown Wars Model(.wbl/.mdl)")
+    self.layout.operator(ImportWBLPSPSectorOperator.bl_idname, text="R* Leeds Chinatown Wars Worldblock(.wbl/.mdl)")
 
 def register():
     bpy.utils.register_class(ImportWBLPSPSectorOperator)
